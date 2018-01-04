4614f80aa312ea295a115a6e894c2db572e96963
Pavel Shchukin <30902662+schukinp@users.noreply.github.com>
2018-01-04 16:52:11 +0300

    Script is created

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
 # Password Strength Calculator
 
-[TODO. There will be project description]
+The script calculates and prints the score of the password strength (from 1 to 10)
 
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+
+# Calculation logic
+
+Password scores:
+* only 1 point if lenght = 1
+* 2 points if lenght > 6
+* 2 points if it has lower chars
+* 2 points if it has upper chars
+* 2 points if it has digits
+* 2 points if it has spacial chars (%@! etc)
+
+# Quickstart
+
+Example of script launch on Linux, Python 3.5:
+```
+$ python password_strenght.py
+Please enter password: Qwerty1@
+Your password strength is: 10
+```
+
--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,31 @@
+import re
 
 
 def get_password_strength(password):
-    pass
+    score = 0
+    if len(password) == 1:
+        score +=1
+        return score
+    if len(password) > 6:
+        score += 2
+    if re.search('[A-Z]', password):
+        score += 2
+    if re.search('[a-z]', password):
+        score += 2
+    if re.search('\d', password):
+        score += 2
+    if re.search('[^A-Za-z\d]', password):
+        score += 2
+    return score
 
 
 if __name__ == '__main__':
-    pass
+    password = input('Please enter password: ')
+    while len(password) == 0:
+        print('Password must be at least 1 characters long!')
+        password = input('Please enter password: ')
+    print('Your password strength is: {}'.format(get_password_strength(password)))
+
+
+
+
